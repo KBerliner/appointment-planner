23cda142745e4b850e30aa333db99a1debd133d6
Kyle <berlinerkyle@gmail.com>
2024-01-16 12:45:46 -0700

    Added the form element for the Contact Form.

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,14 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={({ target }) => setName(target.value)} value={name} type='text' />
+        <input onChange={({ target }) => setPhone(target.value)} value={phone} type='tel' title="Please enter a valid phone number" pattern='[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}' />
+        <input onChange={({ target }) => setEmail(target.value)} value={email} type='email' />
+        <input type='submit' />
+      </form>
+    </>
   );
 };
 

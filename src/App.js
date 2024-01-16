eb9b73e95321975b00ecef08cfa57c2f62553925
Kyle <berlinerkyle@gmail.com>
2024-01-16 11:56:27 -0700

    Added the callback functions to add a new contact and appointment. They also check for duplicate contacts or appointments.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -17,6 +17,38 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+
+  const makeNewContact = (name, phoneNumber, email) => {
+
+    // Checking for empty fields
+    if (!name || !phoneNumber || !email) {
+      alert('All fields must be filled out!');
+      return;
+    }
+
+    // Checking for duplicate contact
+    if (contacts.find((contact) => contact.name === name && contact.phoneNumber === phoneNumber && contact.email === email) !== undefined) {
+      alert('This contact is already in the system!');
+    } else {
+      setContacts(prev => [...prev, { "name": name, "phoneNumber": phoneNumber, "email": email }]);
+    }
+  }
+
+  const makeNewAppointment = (name, contact, date, time) => {
+
+    // Checking for empty fields
+    if (!name || !contact || !date || !time) {
+      alert('All fields must be filled out!');
+      return;
+    }
+
+    // Checking for duplicate appointment
+    if (appointments.find((appointment) => appointment.name === name && appointment.contact === contact && appointment.date === date && appointment.time === time) !== undefined) {
+      alert('This appointment is already in the system!');
+    } else {
+      setAppointments(prev => [...prev, { "name": name, "contact": contact, "date": date, "time": time }]);
+    }
+  }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
